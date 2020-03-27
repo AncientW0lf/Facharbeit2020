f2ab3e55d2fe9115a15ef21360ad326c6e418591
LunarDiver <25Prozent@gmail.com>
2020-03-27 15:59:59 +0100

    Added base files of Access library.

---
+++ AccessCommunication/QueryResult.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Data.OleDb;
+
+namespace AccessCommunication
+{
+    public struct QueryResult
+    {
+        public string ExecutedQuery { get; internal set; }
+
+        public int RecordsAffected { get; internal set; }
+
+        public object[,] ReturnedRows { get; internal set; }
+
+        public QueryResult(string query, OleDbDataReader dataReader)
+        {
+            if(dataReader.IsClosed) throw new ArgumentException("Data reader is closed!");
+
+            ExecutedQuery = query;
+            RecordsAffected = dataReader.RecordsAffected;
+
+            if(dataReader.HasRows)
+            {
+                ReturnedRows = new object[dataReader.FieldCount,dataReader.RecordsAffected];
+
+                int rowCounter = 0;
+                while(dataReader.Read())
+                {
+                    for(int i = 0; i < dataReader.FieldCount; i++)
+                    {
+                        ReturnedRows[i, rowCounter] = dataReader[i];
+                    }
+                }
+            }
+            else
+            {
+                ReturnedRows = null;
+            }
+        }
+    }
+}
