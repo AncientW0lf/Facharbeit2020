6c299bea56c695c13b1c0a392c3cdf80471e69bb
LunarDiver <25Prozent@gmail.com>
2020-04-01 14:32:23 +0200

    Changed BoxLib to PackageReference, added new GUI project with basic structure.

---
+++ FoodPlanner/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using BoxLib.Scripts;
+
+namespace FoodPlanner
+{
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : Application
+	{
+		private void App_OnStartup(object sender, StartupEventArgs e)
+		{
+			LanguageHelper.Resources = typeof(Resources);
+		}
+	}
+}
