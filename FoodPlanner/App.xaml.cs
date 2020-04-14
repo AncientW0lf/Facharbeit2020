3c4849b4626d9d0cd9292c1c69ed6bc464e1a9d9
LunarDiver <25Prozent@gmail.com>
2020-04-14 15:39:20 +0200

    Added exit and unhandled exception events to GUI.

--- FoodPlanner/App.xaml.cs
+++ FoodPlanner/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System.Diagnostics;
+using System.Windows;
+using System.Windows.Threading;
 using BoxLib.Scripts;
 
 namespace FoodPlanner
@@ -11,6 +13,18 @@ namespace FoodPlanner
 		private void App_OnStartup(object sender, StartupEventArgs e)
 		{
 			LanguageHelper.Resources = typeof(Languages.Resources);
+		}
+
+		private void App_OnExit(object sender, ExitEventArgs e)
+		{
+			Log.Close();
+		}
+
+		private void App_OnDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+		{
+			Log.Write("Unhandled exception!", 1, TraceEventType.Critical, true);
+			Log.Write($"Exception: {e.Exception.Message}\n{e.Exception.StackTrace}", 2, null);
+			App_OnExit(sender, null);
 		}
 	}
 }
