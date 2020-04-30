e681febd5be3f9704aba4eb15c869222792a97d1
LunarDiver <25Prozent@gmail.com>
2020-04-30 11:24:55 +0200

    Implemented ChangeLangWin behaviour.

--- FoodPlanner/AdditionalWindows/ChangeLangWin.xaml.cs
+++ FoodPlanner/AdditionalWindows/ChangeLangWin.xaml.cs
@@ -1,15 +1,11 @@
-﻿using System;
+﻿using BoxLib.Scripts;
+using System;
 using System.Collections.Generic;
-using System.Globalization;
-using System.Text;
+using System.Diagnostics;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using CultureInfo = System.Globalization.CultureInfo;
 
 namespace FoodPlanner.AdditionalWindows
 {
@@ -18,9 +14,51 @@ namespace FoodPlanner.AdditionalWindows
 	/// </summary>
 	public partial class ChangeLangWin : Window
 	{
-		public ChangeLangWin(CultureInfo[] languages)
+		/// <summary>
+		/// The culture that was selected by the user.
+		/// </summary>
+		public CultureInfo SelectedCulture { get; private set; }
+
+		/// <summary>
+		/// Initializes a new window and retrieves all supported languages.
+		/// </summary>
+		public ChangeLangWin(IReadOnlyList<CultureInfo> languages)
 		{
 			InitializeComponent();
+
+			//Adds the languages
+			for(int i = 0; i < languages.Count; i++)
+			{
+				LangBox.Items.Add(languages[i]);
+			}
+
+			LangBox.SelectedIndex = 0;
+
+			Log.Write($"Initialized {nameof(ChangeLangWin)} window.", 1, TraceEventType.Information);
+		}
+
+		/// <summary>
+		/// Closes the window and saves the selected language to <see cref="SelectedCulture"/>.
+		/// </summary>
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			//Cancels if nothing was chosen
+			if(string.IsNullOrWhiteSpace(LangBox.Text))
+				return;
+
+			SelectedCulture = LangBox.SelectedItem as CultureInfo;
+			DialogResult = true;
+			Close();
+		}
+
+		private void ChangeFlag(object sender, SelectionChangedEventArgs e)
+		{
+			var selectedItem = e.AddedItems[0] as CultureInfo;
+
+			if(selectedItem.Equals(CultureInfo.GetCultureInfo("en-us")))
+				FlagImg.Source = new BitmapImage(new Uri(@"Resources\Flags\united-states.png"));
+			else if(selectedItem.Equals(CultureInfo.GetCultureInfo("de-de")))
+				FlagImg.Source = new BitmapImage(new Uri(@"Resources\Flags\germany.png"));
 		}
 	}
 }
