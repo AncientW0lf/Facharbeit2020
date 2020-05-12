4d4643f688e64a3cd43ac8895683e3241fd4ff36
LunarDiver <25Prozent@gmail.com>
2020-05-12 15:21:03 +0200

    Implemented basic recipe example.

--- FoodPlanner/Pages/RecipesPage.xaml.cs
+++ FoodPlanner/Pages/RecipesPage.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System.Windows;
+﻿using AccessCommunication;
 using System.Windows.Controls;
 
 namespace FoodPlanner.Pages
@@ -15,7 +15,26 @@ namespace FoodPlanner.Pages
 		{
 			InitializeComponent();
 
-			MessageBox.Show(App.AccessDB.ExecuteQuery("select * from Wochenplan").ReturnedRows.Count.ToString());
+			Dispatcher.InvokeAsync(async() =>
+			{
+				QueryResult recipes = await App.AccessDB.ExecuteQuery("select Gerichtname, Zubereitung from Rezepte");
+
+				ListRecipes.Items.Clear();
+				for(int i = 0; i < recipes.ReturnedRows.Count; i++)
+				{
+					ListRecipes.Items.Add(recipes.ReturnedRows[i][0]);
+				}
+
+				TxtBoxRecipeName.Text = recipes.ReturnedRows[0][0].ToString() ?? "";
+				TxtBoxRecipeDesc.Text = recipes.ReturnedRows[0][1].ToString() ?? "";
+
+				QueryResult ingredients = await App.AccessDB.ExecuteQuery("select * from Zutatenmengen");
+				ListIngreds.Items.Clear();
+				for(int i = 1; i < ingredients.ColumnNames.Length; i++)
+				{
+					ListIngreds.Items.Add($"{ingredients.ReturnedRows[0][i]} {ingredients.ColumnNames[i]}");
+				}
+			});
 		}
 	}
 }
