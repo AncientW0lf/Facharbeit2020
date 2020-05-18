ec92cba9b3da2b5f25e4f1433e6e8bd7a36569f3
LunarDiver <25Prozent@gmail.com>
2020-05-18 11:53:23 +0200

    Fixed ingredients not showing correctly.

--- FoodPlanner/Pages/RecipesPage.xaml.cs
+++ FoodPlanner/Pages/RecipesPage.xaml.cs
@@ -23,15 +23,17 @@ namespace FoodPlanner.Pages
 
 			Task.Run(async() =>
 			{
-				_recipes = await App.AccessDB.ExecuteQuery("select Gerichtname, Zubereitung from Rezepte");
-				_ingredients = await App.AccessDB.ExecuteQuery("select * from Zutatenmengen");
+				_recipes = await App.AccessDB.ExecuteQuery(
+					"select ID, Gerichtname, Zubereitung from Rezepte");
+				_ingredients = await App.AccessDB.ExecuteQuery(
+					"select Rezepte_ID, Gerichtname, Zutat, Menge, Notiz from ZusammenfassungRezeptzutatenliste");
 
 				await Dispatcher.InvokeAsync(() =>
 				{
 					ListRecipes.Items.Clear();
 					for(int i = 0; i < _recipes.ReturnedRows.Count; i++)
 					{
-						ListRecipes.Items.Add(_recipes.ReturnedRows[i][0]);
+						ListRecipes.Items.Add(_recipes.ReturnedRows[i][1]);
 					}
 				});
 			});
@@ -39,19 +41,25 @@ namespace FoodPlanner.Pages
 
 		private void ListRecipes_SelectionChanged(object sender, SelectionChangedEventArgs e)
 		{
-			object[] selected = _recipes.ReturnedRows.FirstOrDefault(a => a[0].Equals(e.AddedItems[0]));
-			int selectedIndex = _recipes.ReturnedRows.IndexOf(selected);
+			object[] selected = _recipes.ReturnedRows.FirstOrDefault(a => a[1].Equals(e.AddedItems[0]));
+			object[][] selectedIngreds = _ingredients.ReturnedRows.Where(a => a[0].Equals(selected?[0])).ToArray();
 
 			if(selected == null)
 				return;
 
-			TxtBoxRecipeName.Text = _recipes.ReturnedRows[selectedIndex][0].ToString() ?? "";
-			TxtBoxRecipeDesc.Text = _recipes.ReturnedRows[selectedIndex][1].ToString() ?? "";
+			TxtBoxRecipeName.Text = selected[1].ToString() ?? "";
+			TxtBoxRecipeDesc.Text = selected[2].ToString() ?? "";
 
 			ListIngreds.Items.Clear();
-			for(int i = 1; i < _ingredients.ColumnNames.Length; i++)
+			for (int i = 0; i < selectedIngreds.Length; i++)
 			{
-				ListIngreds.Items.Add($"{_ingredients.ReturnedRows[selectedIndex][i]} {_ingredients.ColumnNames[i]}");
+				ListIngreds.Items.Add(new ListBoxItem
+				{
+					Content = $"{selectedIngreds[i][3]} {selectedIngreds[i][2]}"
+				});
+
+				if(!string.IsNullOrWhiteSpace(selectedIngreds[i][4].ToString()))
+					((ListBoxItem)ListIngreds.Items[^1]).ToolTip = selectedIngreds[i][4];
 			}
 		}
 	}
