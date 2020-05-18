0b51048cc815b7cdbe879d7a72e39c245f172032
LunarDiver <25Prozent@gmail.com>
2020-05-18 10:03:25 +0200

    Removed placeholder items, added select behaviour for recipes.

--- FoodPlanner/Pages/RecipesPage.xaml.cs
+++ FoodPlanner/Pages/RecipesPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using AccessCommunication;
+﻿using System.Linq;
+using System.Threading.Tasks;
+using AccessCommunication;
 using System.Windows.Controls;
 
 namespace FoodPlanner.Pages
@@ -8,6 +10,10 @@ namespace FoodPlanner.Pages
 	/// </summary>
 	public partial class RecipesPage : Page
 	{
+		private QueryResult _recipes;
+
+		private QueryResult _ingredients;
+
 		/// <summary>
 		/// Initialize this page.
 		/// </summary>
@@ -15,26 +21,38 @@ namespace FoodPlanner.Pages
 		{
 			InitializeComponent();
 
-			Dispatcher.InvokeAsync(async() =>
+			Task.Run(async() =>
 			{
-				QueryResult recipes = await App.AccessDB.ExecuteQuery("select Gerichtname, Zubereitung from Rezepte");
+				_recipes = await App.AccessDB.ExecuteQuery("select Gerichtname, Zubereitung from Rezepte");
+				_ingredients = await App.AccessDB.ExecuteQuery("select * from Zutatenmengen");
 
-				ListRecipes.Items.Clear();
-				for(int i = 0; i < recipes.ReturnedRows.Count; i++)
+				await Dispatcher.InvokeAsync(() =>
 				{
-					ListRecipes.Items.Add(recipes.ReturnedRows[i][0]);
-				}
-
-				TxtBoxRecipeName.Text = recipes.ReturnedRows[0][0].ToString() ?? "";
-				TxtBoxRecipeDesc.Text = recipes.ReturnedRows[0][1].ToString() ?? "";
-
-				QueryResult ingredients = await App.AccessDB.ExecuteQuery("select * from Zutatenmengen");
-				ListIngreds.Items.Clear();
-				for(int i = 1; i < ingredients.ColumnNames.Length; i++)
-				{
-					ListIngreds.Items.Add($"{ingredients.ReturnedRows[0][i]} {ingredients.ColumnNames[i]}");
-				}
+					ListRecipes.Items.Clear();
+					for(int i = 0; i < _recipes.ReturnedRows.Count; i++)
+					{
+						ListRecipes.Items.Add(_recipes.ReturnedRows[i][0]);
+					}
+				});
 			});
+		}
+
+		private void ListRecipes_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			object[] selected = _recipes.ReturnedRows.FirstOrDefault(a => a[0].Equals(e.AddedItems[0]));
+			int selectedIndex = _recipes.ReturnedRows.IndexOf(selected);
+
+			if(selected == null)
+				return;
+
+			TxtBoxRecipeName.Text = _recipes.ReturnedRows[selectedIndex][0].ToString() ?? "";
+			TxtBoxRecipeDesc.Text = _recipes.ReturnedRows[selectedIndex][1].ToString() ?? "";
+
+			ListIngreds.Items.Clear();
+			for(int i = 1; i < _ingredients.ColumnNames.Length; i++)
+			{
+				ListIngreds.Items.Add($"{_ingredients.ReturnedRows[selectedIndex][i]} {_ingredients.ColumnNames[i]}");
+			}
 		}
 	}
 }
