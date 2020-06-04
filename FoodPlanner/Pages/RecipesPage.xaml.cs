56217de4dbe3ed1b4b228cc7697b90f6f513daf6
LunarDiver <25Prozent@gmail.com>
2020-06-04 09:26:00 +0200

    Moved recipe creation code to new class.

--- FoodPlanner/Pages/RecipesPage.xaml.cs
+++ FoodPlanner/Pages/RecipesPage.xaml.cs
@@ -75,46 +75,7 @@ namespace FoodPlanner.Pages
 
 		private async void OpenNewRecipeWin(object sender, RoutedEventArgs e)
 		{
-			var win = new CreateRecipeWin();
-			win.ShowDialog();
-
-			if(win.DialogResult != true)
-				return;
-
-			var allResults = new List<QueryResult>
-			{
-				await App.ExecuteQuery(
-					"insert into Rezepte(Gerichtname, Zubereitung) " +
-					$"values('{win.Recipe.Name}', '{win.Recipe.Preparation}')"),
-				await App.ExecuteQuery("select ID from Rezepte " +
-				                       $"where Gerichtname = '{win.Recipe.Name}' " +
-				                       $"and Zubereitung = '{win.Recipe.Preparation}'")
-			};
-
-			if(allResults[1].ReturnedRows?[0][0] is int newRecipeID)
-			{
-				for (int i = 0; i < win.Recipe.LinkedIngredients.Count; i++)
-				{
-					allResults.Add(await App.ExecuteQuery(
-						"insert into Rezeptzutatenliste(IDRezepte, IDZutaten, Menge, Notiz) " +
-						$"values({newRecipeID}, " +
-						$"{win.Recipe.LinkedIngredients[i].ID}, " +
-						$"'{win.Recipe.LinkedIngredients[i].Amount}', " +
-						$"'{win.Recipe.LinkedIngredients[i].Note}')"));
-				}
-			}
-
-			if(!allResults.All(a => a.Success))
-			{
-				MessageBox.Show(Languages.Resources.MsgRecipeCreatedWithErrors
-						.Replace("$1", allResults.Count(a => !a.Success).ToString()), 
-					Languages.Resources.ErrorSimple, MessageBoxButton.OK, MessageBoxImage.Error);
-			}
-			else
-			{
-				MessageBox.Show(Languages.Resources.MsgRecipeCreated, 
-					Languages.Resources.SuccessSimple, MessageBoxButton.OK, MessageBoxImage.Information);
-			}
+			await SqlHelper.InsertNewRecipeInteractive(true);
 
 			NavigationService?.Refresh();
 		}
@@ -126,42 +87,7 @@ namespace FoodPlanner.Pages
 			if(selected == null) 
 				return;
 
-			var win = new CreateRecipeWin((int)selected[0]);
-			win.ShowDialog();
-
-			if(win.DialogResult != true)
-				return;
-
-			var allResults = new List<QueryResult>
-			{
-				await App.ExecuteQuery(
-					"update Rezepte " +
-					$"set Gerichtname = '{win.Recipe.Name}', Zubereitung = '{win.Recipe.Preparation}' " +
-					$"where ID = {win.Recipe.ID}"),
-				await App.ExecuteQuery($"delete from Rezeptzutatenliste where IDRezepte = {win.Recipe.ID}")
-			};
-
-			for (int i = 0; i < win.Recipe.LinkedIngredients.Count; i++)
-			{
-				allResults.Add(await App.ExecuteQuery(
-					"insert into Rezeptzutatenliste(IDRezepte, IDZutaten, Menge, Notiz) " +
-					$"values({win.Recipe.ID}, " +
-					$"{win.Recipe.LinkedIngredients[i].ID}, " +
-					$"'{win.Recipe.LinkedIngredients[i].Amount}', " +
-					$"'{win.Recipe.LinkedIngredients[i].Note}')"));
-			}
-
-			if (!allResults.All(a => a.Success))
-			{
-				MessageBox.Show(Languages.Resources.MsgRecipeUpdatedWithErrors
-						.Replace("$1", allResults.Count(a => !a.Success).ToString()), 
-					Languages.Resources.ErrorSimple, MessageBoxButton.OK, MessageBoxImage.Error);
-			}
-			else
-			{
-				MessageBox.Show(Languages.Resources.MsgRecipeCreated, 
-					Languages.Resources.SuccessSimple, MessageBoxButton.OK, MessageBoxImage.Information);
-			}
+			await SqlHelper.UpdateRecipeInteractive((int)selected[0], true);
 
 			NavigationService?.Refresh();
 		}
@@ -170,27 +96,10 @@ namespace FoodPlanner.Pages
 		{
 			object[] selected = _recipes.ReturnedRows.FirstOrDefault(a => a[0].Equals(((FullRecipe)ListRecipes.SelectedItem)?.ID));
 
-			if(selected == null || MessageBox.Show(Languages.Resources.DelRecipeConfirm.Replace("$1", selected[1].ToString()),
-				Languages.Resources.ConfirmSimple, MessageBoxButton.YesNo, MessageBoxImage.Question,
-				MessageBoxResult.No) == MessageBoxResult.No)
+			if(selected == null)
 				return;
 
-			QueryResult result = default, result2 = default;
-			try
-			{
-				result = await App.ExecuteQuery($"delete from Rezeptzutatenliste where IDRezepte = {selected[0]}");
-				result2 = await App.ExecuteQuery($"delete from Rezepte where ID = {selected[0]}");
-			}
-			catch(Exception)
-			{
-				//Ignore
-			}
-
-			MessageBox.Show(Languages.Resources.DelQueriesInfo
-					.Replace("$1", "2")
-					.Replace("$2", (result.Success && result2.Success).ToString(Languages.Resources.Culture))
-					.Replace("$3", (result.RecordsAffected + result2.RecordsAffected).ToString()),
-				Languages.Resources.InfoSimple, MessageBoxButton.OK, MessageBoxImage.Information);
+			await SqlHelper.DeleteRecipeInteractive(selected[1].ToString(), (int)selected[0], true);
 
 			NavigationService?.Refresh();
 		}
---
+++ FoodPlanner/SqlHelper.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using AccessCommunication;
+using FoodPlanner.AdditionalWindows;
+using FoodPlanner.SQLObj;
+
+namespace FoodPlanner
+{
+	public static class SqlHelper
+	{
+		public static async Task<QueryResult[]> InsertNewRecipe(FullRecipe recipe)
+		{
+			var allResults = new List<QueryResult>
+			{
+				await App.ExecuteQuery(
+					"insert into Rezepte(Gerichtname, Zubereitung) " +
+					$"values('{recipe.Name}', '{recipe.Preparation}')"),
+				await App.ExecuteQuery("select ID from Rezepte " +
+				                       $"where Gerichtname = '{recipe.Name}' " +
+				                       $"and Zubereitung = '{recipe.Preparation}'")
+			};
+
+			if(allResults[1].ReturnedRows?[0][0] is int newRecipeID)
+			{
+				for (int i = 0; i < recipe.LinkedIngredients.Count; i++)
+				{
+					allResults.Add(await App.ExecuteQuery(
+						"insert into Rezeptzutatenliste(IDRezepte, IDZutaten, Menge, Notiz) " +
+						$"values({newRecipeID}, " +
+						$"{recipe.LinkedIngredients[i].ID}, " +
+						$"'{recipe.LinkedIngredients[i].Amount}', " +
+						$"'{recipe.LinkedIngredients[i].Note}')"));
+				}
+			}
+
+			return allResults.ToArray();
+		}
+
+		public static async Task<QueryResult[]> InsertNewRecipeInteractive(bool msgFinished)
+		{
+			var win = new CreateRecipeWin();
+			win.ShowDialog();
+
+			if(win.DialogResult != true)
+				return null;
+
+			QueryResult[] allResults = await InsertNewRecipe(win.Recipe);
+
+			if(!msgFinished) return allResults;
+
+			if(!allResults.All(a => a.Success))
+			{
+				MessageBox.Show(Languages.Resources.MsgRecipeCreatedWithErrors
+						.Replace("$1", allResults.Count(a => !a.Success).ToString()), 
+					Languages.Resources.ErrorSimple, MessageBoxButton.OK, MessageBoxImage.Error);
+			}
+			else
+			{
+				MessageBox.Show(Languages.Resources.MsgRecipeCreated, 
+					Languages.Resources.SuccessSimple, MessageBoxButton.OK, MessageBoxImage.Information);
+			}
+
+			return allResults;
+		}
+
+		public static async Task<QueryResult[]> UpdateRecipe(FullRecipe recipe)
+		{
+			var allResults = new List<QueryResult>
+			{
+				await App.ExecuteQuery(
+					"update Rezepte " +
+					$"set Gerichtname = '{recipe.Name}', Zubereitung = '{recipe.Preparation}' " +
+					$"where ID = {recipe.ID}"),
+				await App.ExecuteQuery($"delete from Rezeptzutatenliste where IDRezepte = {recipe.ID}")
+			};
+
+			for (int i = 0; i < recipe.LinkedIngredients.Count; i++)
+			{
+				allResults.Add(await App.ExecuteQuery(
+					"insert into Rezeptzutatenliste(IDRezepte, IDZutaten, Menge, Notiz) " +
+					$"values({recipe.ID}, " +
+					$"{recipe.LinkedIngredients[i].ID}, " +
+					$"'{recipe.LinkedIngredients[i].Amount}', " +
+					$"'{recipe.LinkedIngredients[i].Note}')"));
+			}
+
+			return allResults.ToArray();
+		}
+
+		public static async Task<QueryResult[]> UpdateRecipeInteractive(int recipeId, bool msgFinished)
+		{
+			var win = new CreateRecipeWin(recipeId);
+			win.ShowDialog();
+
+			if(win.DialogResult != true)
+				return null;
+
+			QueryResult[] allResults = await UpdateRecipe(win.Recipe);
+
+			if(!msgFinished) return allResults;
+
+			if (!allResults.All(a => a.Success))
+			{
+				MessageBox.Show(Languages.Resources.MsgRecipeUpdatedWithErrors
+						.Replace("$1", allResults.Count(a => !a.Success).ToString()), 
+					Languages.Resources.ErrorSimple, MessageBoxButton.OK, MessageBoxImage.Error);
+			}
+			else
+			{
+				MessageBox.Show(Languages.Resources.MsgRecipeCreated, 
+					Languages.Resources.SuccessSimple, MessageBoxButton.OK, MessageBoxImage.Information);
+			}
+
+			return allResults;
+		}
+
+		public static async Task<QueryResult[]> DeleteRecipe(int recipeId)
+		{
+			QueryResult result = default, result2 = default;
+			try
+			{
+				result = await App.ExecuteQuery($"delete from Rezeptzutatenliste where IDRezepte = {recipeId}");
+				result2 = await App.ExecuteQuery($"delete from Rezepte where ID = {recipeId}");
+			}
+			catch(Exception)
+			{
+				//Ignore
+			}
+
+			return new[] {result, result2};
+		}
+
+		public static async Task<QueryResult[]> DeleteRecipeInteractive(string recipeName, int recipeId, bool msgFinished)
+		{
+			if(MessageBox.Show(Languages.Resources.DelRecipeConfirm.Replace("$1", recipeName),
+				   Languages.Resources.ConfirmSimple, MessageBoxButton.YesNo, MessageBoxImage.Question,
+				   MessageBoxResult.No) == MessageBoxResult.No)
+				return null;
+
+			QueryResult[] results = await DeleteRecipe(recipeId);
+
+			if(msgFinished)
+			{
+				MessageBox.Show(Languages.Resources.DelQueriesInfo
+					.Replace("$1", "2")
+					.Replace("$2", (results[0].Success && results[1].Success).ToString(Languages.Resources.Culture))
+					.Replace("$3", (results[0].RecordsAffected + results[1].RecordsAffected).ToString()),
+				Languages.Resources.InfoSimple, MessageBoxButton.OK, MessageBoxImage.Information);
+			}
+
+			return results;
+		}
+	}
+}
